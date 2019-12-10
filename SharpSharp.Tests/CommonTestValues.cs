606bd1ee390e0977b46ebfb4d9f3b85a86ade2d1
Randy Ridge <randyridge@gmail.com>
2019-12-09 20:58:49 -0500

    get started on tests (#4)

---
+++ SharpSharp.Tests/CommonTestValues.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SharpSharp {
+    public static class CommonTestValues {
+        private const string Murray = @"https://www.fillmurray.com/300/300";
+        public const string ImageUrl = Murray;
+        public static readonly Uri ImageUri = new Uri(Murray);
+        public static readonly ImageLoadOptions DefaultImageLoadOptions = new ImageLoadOptions();
+    }
+}
