c475eca638612ff479811b2b31d0a9443fd3082c
Randy Ridge <randyridge@gmail.com>
2019-12-09 20:58:20 -0500

    get started on tests

---
+++ SharpSharp.Tests/ImagePipelineTester.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using RandyRidge.Common;
+using Shouldly;
+using Xunit;
+
+namespace SharpSharp {
+    public static class ImagePipelineTester {
+        public static class FromUriAsync {
+            [Fact]
+            public static async Task returns_pipeline() => (await ImagePipeline.FromUriAsync(CommonTestValues.ImageUrl, new ImageLoadOptions()).ForAwait()).ShouldNotBeNull();
+
+            [Fact]
+            public static Task throws_on_null_options() => Should.ThrowAsync<ArgumentNullException>(() => ImagePipeline.FromUriAsync(CommonTestValues.ImageUri, null));
+
+            [Fact]
+            public static Task throws_on_null_uri() => Should.ThrowAsync<ArgumentNullException>(() => ImagePipeline.FromUriAsync((Uri) null, CommonTestValues.DefaultImageLoadOptions));
+        }
+    }
+}
--- SharpSharp.Tests/TestImageData.cs
+++ SharpSharp.Tests/TestImageData.cs
@@ -2,8 +2,8 @@
 
 namespace SharpSharp.Tests {
     public static class TestImageData {
-        public static string InputJpg = BuildPath("2569067123_aca715a2ee_o.jpg");  // http://www.flickr.com/photos/grizdave/2569067123/
-        public static string OutputJpg = BuildPath("output.jpg");
+        public static readonly string InputJpg = BuildPath("2569067123_aca715a2ee_o.jpg");  // http://www.flickr.com/photos/grizdave/2569067123/
+        public static readonly string OutputJpg = BuildPath("output.jpg");
 
         private static string BuildPath(string fileName) => Path.Join("Images", fileName);
     }
