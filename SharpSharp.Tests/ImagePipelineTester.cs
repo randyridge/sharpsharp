744a7ce48d84fcfb0d8c6390a1cafc21775ea373
Randy Ridge <randyridge@gmail.com>
2019-12-11 14:02:02 -0500

    add sharp's test images

--- SharpSharp.Tests/ImagePipelineTester.cs
+++ SharpSharp.Tests/ImagePipelineTester.cs
@@ -8,13 +8,43 @@ namespace SharpSharp {
     public static class ImagePipelineTester {
         public static class FromUriAsync {
             [Fact]
-            public static async Task returns_pipeline() => (await ImagePipeline.FromUriAsync(CommonTestValues.ImageUrl, new ImageLoadOptions()).ForAwait()).ShouldNotBeNull();
+            public static async Task returns_pipeline() => (await ImagePipeline.FromUriAsync(CommonTestValues.ImageUri).ForAwait()).ShouldNotBeNull();
+
+            [Fact]
+            public static Task throws_on_null_uri() => Should.ThrowAsync<ArgumentNullException>(() => ImagePipeline.FromUriAsync((Uri) null));
+        }
+
+        public static class FromUriAsyncWithOptions {
+            [Fact]
+            public static async Task returns_pipeline() => (await ImagePipeline.FromUriAsync(CommonTestValues.ImageUri, new ImageLoadOptions()).ForAwait()).ShouldNotBeNull();
 
             [Fact]
             public static Task throws_on_null_options() => Should.ThrowAsync<ArgumentNullException>(() => ImagePipeline.FromUriAsync(CommonTestValues.ImageUri, null));
 
             [Fact]
             public static Task throws_on_null_uri() => Should.ThrowAsync<ArgumentNullException>(() => ImagePipeline.FromUriAsync((Uri) null, CommonTestValues.DefaultImageLoadOptions));
+        }
+
+        public static class FromUriAsyncWithString {
+            [Fact]
+            public static async Task returns_pipeline() => (await ImagePipeline.FromUriAsync(CommonTestValues.ImageUrl, new ImageLoadOptions()).ForAwait()).ShouldNotBeNull();
+
+            [Fact]
+            public static Task throws_on_null_options() => Should.ThrowAsync<ArgumentNullException>(() => ImagePipeline.FromUriAsync(CommonTestValues.ImageUrl, null));
+
+            [Fact]
+            public static Task throws_on_null_uri() => Should.ThrowAsync<ArgumentNullException>(() => ImagePipeline.FromUriAsync((string) null, CommonTestValues.DefaultImageLoadOptions));
+        }
+
+        public static class FromUriAsyncWithStringAndOptions {
+            [Fact]
+            public static async Task returns_pipeline() => (await ImagePipeline.FromUriAsync(CommonTestValues.ImageUrl, new ImageLoadOptions()).ForAwait()).ShouldNotBeNull();
+
+            [Fact]
+            public static Task throws_on_null_options() => Should.ThrowAsync<ArgumentNullException>(() => ImagePipeline.FromUriAsync(CommonTestValues.ImageUrl, null));
+
+            [Fact]
+            public static Task throws_on_null_uri() => Should.ThrowAsync<ArgumentNullException>(() => ImagePipeline.FromUriAsync((string) null, CommonTestValues.DefaultImageLoadOptions));
         }
     }
 }
