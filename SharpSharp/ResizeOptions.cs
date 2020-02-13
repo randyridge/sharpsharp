877f16a46b4bda1ba94b37de441bf1ec1f0daa6f
Randy Ridge <randyridge@gmail.com>
2020-02-13 16:51:46 -0500

    add the contain/embed functionality

--- SharpSharp/ResizeOptions.cs
+++ SharpSharp/ResizeOptions.cs
@@ -2,8 +2,10 @@
 
 namespace SharpSharp {
     public sealed class ResizeOptions {
+
         // TODO: background
-        public ResizeOptions(int? width = -1, int? height = -1, Fit fit = Fit.Cover, CoverBehavior behavior = CoverBehavior.Center, string kernel = Enums.Kernel.Lanczos3, bool withoutEnlargement = false, bool fastShrinkOnLoad = true) {
+        public ResizeOptions(int? width = -1, int? height = -1, Fit fit = Fit.Cover, CoverBehavior behavior = CoverBehavior.Center,
+            Rgba? background = null, string kernel = Enums.Kernel.Lanczos3, bool withoutEnlargement = false, bool fastShrinkOnLoad = true) {
             // TODO: this
             //Guard.MinimumExclusive(width, 0, nameof(width));
             //Guard.MinimumExclusive(height, 0, nameof(height));
@@ -11,10 +13,13 @@ namespace SharpSharp {
             Height = height ?? -1;
             Fit = fit;
             Behavior = behavior;
+            Background = background ?? new Rgba(0, 0, 0, 1);
             Kernel = kernel;
             WithoutEnlargement = withoutEnlargement;
             FastShrinkOnLoad = fastShrinkOnLoad;
         }
+
+        public Rgba Background { get; }
 
         public CoverBehavior Behavior { get; }
 
---
+++ SharpSharp/Rgba.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SharpSharp {
+    public readonly struct Rgba : IEquatable<Rgba> {
+        public Rgba(byte red, byte green, byte blue, byte alpha) {
+            Red = red;
+            Green = green;
+            Blue = blue;
+            Alpha = alpha;
+        }
+
+        public byte Red { get; }
+
+        public byte Green { get; }
+
+        public byte Blue { get; }
+
+        public byte Alpha { get; }
+
+        public bool Equals(Rgba other) => Red == other.Red && Green == other.Green && Blue == other.Blue && Alpha.Equals(other.Alpha);
+
+        public override bool Equals(object? obj) => obj is Rgba other && Equals(other);
+
+        public override int GetHashCode() => HashCode.Combine(Red, Green, Blue, Alpha);
+
+        public double[] ToDoubles() => new double[] {Red, Green, Blue, Alpha};
+
+        public static bool operator ==(Rgba left, Rgba right) => left.Equals(right);
+
+        public static bool operator !=(Rgba left, Rgba right) => !left.Equals(right);
+    }
+}
