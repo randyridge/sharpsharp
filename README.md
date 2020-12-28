16dad5ad6f3f33f9cf43eb4e5c0d5f3c70ea3000
Randy Ridge <randyridge@gmail.com>
2020-12-28 15:17:07 -0500

    update benchmark

--- README.md
+++ README.md
@@ -32,22 +32,27 @@ namespace Demo {
 
 ## Benchmark
 ```
+
 BenchmarkDotNet=v0.12.1, OS=Windows 10.0.19041.685 (2004/?/20H1)
 Intel Core i7-2600K CPU 3.40GHz (Sandy Bridge), 1 CPU, 8 logical and 4 physical cores
 .NET Core SDK=5.0.101
   [Host]     : .NET Core 5.0.1 (CoreCLR 5.0.120.57516, CoreFX 5.0.120.57516), X64 RyuJIT
-  Job-AAXGPA : .NET Core 5.0.1 (CoreCLR 5.0.120.57516, CoreFX 5.0.120.57516), X64 RyuJIT
+  Job-YJAIQO : .NET Core 5.0.1 (CoreCLR 5.0.120.57516, CoreFX 5.0.120.57516), X64 RyuJIT
 
 Runtime=.NET Core 5.0
 
-|                     Method |       Mean |     Error |     StdDev |    StdErr |        Min |         Q1 |     Median |         Q3 |        Max |    Op/s | Ratio | RatioSD |    Gen 0 |    Gen 1 |    Gen 2 | Allocated |
-|--------------------------- |-----------:|----------:|-----------:|----------:|-----------:|-----------:|-----------:|-----------:|-----------:|--------:|------:|--------:|---------:|---------:|---------:|----------:|
-|                 ImageSharp | 178.158 ms | 3.4904 ms |  5.9270 ms | 0.9744 ms | 169.338 ms | 174.704 ms | 176.968 ms | 181.595 ms | 193.143 ms |   5.613 | 35.69 |    1.58 |        - |        - |        - |  53.44 KB |
-|     MagickFromBufferToFile | 410.957 ms | 7.7890 ms |  7.2858 ms | 1.8812 ms | 397.375 ms | 404.889 ms | 412.754 ms | 416.415 ms | 420.528 ms |   2.433 | 81.58 |    2.39 |        - |        - |        - |  18.45 KB |
-|       MagickFromFileToFile | 412.873 ms | 8.1176 ms | 11.3797 ms | 2.1900 ms | 384.312 ms | 406.413 ms | 415.170 ms | 420.213 ms | 432.843 ms |   2.422 | 83.05 |    3.76 |        - |        - |        - |  17.31 KB |
-|     MagickFromStreamToFile | 413.492 ms | 8.0051 ms | 11.2221 ms | 2.1597 ms | 394.712 ms | 405.396 ms | 411.045 ms | 421.273 ms | 436.576 ms |   2.418 | 83.16 |    3.29 |        - |        - |        - |   30.7 KB |
-| SharpSharpFromBufferToFile |   4.996 ms | 0.0990 ms |  0.1708 ms | 0.0277 ms |   4.675 ms |   4.855 ms |   4.972 ms |   5.101 ms |   5.447 ms | 200.166 |  1.00 |    0.00 | 328.1250 | 328.1250 | 328.1250 |  10.69 KB |
-|   SharpSharpFromFileToFile |   5.454 ms | 0.1043 ms |  0.1827 ms | 0.0293 ms |   5.171 ms |   5.337 ms |   5.477 ms |   5.570 ms |   5.870 ms | 183.368 |  1.09 |    0.05 |        - |        - |        - |   11.2 KB |
-| SharpSharpFromStreamToFile |   5.366 ms | 0.1054 ms |  0.1213 ms | 0.0271 ms |   5.100 ms |   5.277 ms |   5.350 ms |   5.434 ms |   5.586 ms | 186.364 |  1.07 |    0.04 |        - |        - |        - |  13.99 KB |
+|                         Method |       Mean |     Error |     StdDev |    StdErr |        Min |         Q1 |     Median |         Q3 |        Max |    Op/s |  Ratio | RatioSD |     Gen 0 |     Gen 1 |     Gen 2 |  Allocated |
+|------------------------------- |-----------:|----------:|-----------:|----------:|-----------:|-----------:|-----------:|-----------:|-----------:|--------:|-------:|--------:|----------:|----------:|----------:|-----------:|
+|  'SharpSharp Buffer to Buffer' |   3.975 ms | 0.0786 ms |  0.1356 ms | 0.0220 ms |   3.734 ms |   3.867 ms |   3.960 ms |   4.086 ms |   4.324 ms | 251.561 |   1.00 |    0.00 |  328.1250 |  328.1250 |  328.1250 |   10.04 KB |
+|      'SharpSharp File to File' |   5.302 ms | 0.1036 ms |  0.1518 ms | 0.0282 ms |   4.983 ms |   5.185 ms |   5.298 ms |   5.381 ms |   5.576 ms | 188.622 |   1.33 |    0.06 |         - |         - |         - |   11.38 KB |
+|   'SkiaSharp Buffer to Buffer' | 104.248 ms | 2.0808 ms |  2.1369 ms | 0.5183 ms | 101.145 ms | 102.711 ms | 104.248 ms | 105.266 ms | 108.725 ms |   9.593 |  26.12 |    1.05 |  200.0000 |         - |         - | 1023.89 KB |
+|       'SkiaSharp File to File' | 106.636 ms | 2.0920 ms |  2.4092 ms | 0.5387 ms | 102.911 ms | 104.962 ms | 106.163 ms | 108.947 ms | 110.617 ms |   9.378 |  26.68 |    1.18 |  200.0000 |         - |         - |  964.06 KB |
+|  'ImageSharp Buffer to Buffer' | 195.648 ms | 3.8943 ms |  9.3305 ms | 1.1315 ms | 177.560 ms | 188.953 ms | 193.276 ms | 201.672 ms | 219.222 ms |   5.111 |  49.64 |    3.18 |         - |         - |         - |  368.17 KB |
+|      'ImageSharp File to File' | 197.205 ms | 3.9118 ms |  7.9908 ms | 1.1189 ms | 183.202 ms | 192.083 ms | 196.480 ms | 202.130 ms | 214.361 ms |   5.071 |  49.49 |    2.69 |         - |         - |         - |   53.59 KB |
+|   'FreeImage Buffer to Buffer' | 212.982 ms | 4.2326 ms |  5.3528 ms | 1.1161 ms | 205.365 ms | 209.224 ms | 212.683 ms | 216.850 ms | 225.572 ms |   4.695 |  53.44 |    2.54 | 1000.0000 | 1000.0000 | 1000.0000 |  189.62 KB |
+|       'FreeImage File to File' | 230.281 ms | 4.5688 ms |  5.2614 ms | 1.1765 ms | 222.607 ms | 226.276 ms | 230.792 ms | 233.663 ms | 241.642 ms |   4.343 |  57.62 |    2.78 | 1000.0000 | 1000.0000 | 1000.0000 |   12.82 KB |
+|     'ImageMagick File to File' | 409.113 ms | 7.6457 ms | 13.5903 ms | 2.1488 ms | 384.667 ms | 400.364 ms | 408.498 ms | 417.691 ms | 436.796 ms |   2.444 | 103.06 |    4.07 |         - |         - |         - |   17.31 KB |
+| 'ImageMagick Buffer to Buffer' | 411.799 ms | 8.1147 ms | 12.6336 ms | 2.2333 ms | 386.371 ms | 403.606 ms | 411.966 ms | 420.014 ms | 439.007 ms |   2.428 | 103.59 |    4.87 |         - |         - |         - |  342.49 KB |
+
 ```
 
--- SharpSharp.Benchmarks/SharpBenchmark.cs
+++ SharpSharp.Benchmarks/SharpBenchmark.cs
@@ -1,17 +1,25 @@
 ﻿using System;
 using System.IO;
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using FreeImageAPI;
 using ImageMagick;
 using NetVips;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats.Jpeg;
 using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Processing.Processors.Transforms;
+using SkiaSharp;
+using Image = SixLabors.ImageSharp.Image;
 
 namespace SharpSharp.Benchmarks {
+	// https://sharp.pixelplumbing.com/performance
 	[Config(typeof(DefaultConfig))]
+	[Orderer(SummaryOrderPolicy.FastestToSlowest, MethodOrderPolicy.Alphabetical)]
 	//[EtwProfiler]
 	public class SharpBenchmark {
 		private const int Height = 588;
+		private const int Quality = 80;
 		private const int Width = 720;
 		private readonly byte[] buffer;
 
@@ -22,61 +30,78 @@ namespace SharpSharp.Benchmarks {
 			buffer = File.ReadAllBytes(TestFiles.InputJpg);
 		}
 
-		[Benchmark]
-		public void ImageSharp() {
-			using var output = TestFiles.OutputJpg();
-			using var image = SixLabors.ImageSharp.Image.Load(TestFiles.InputJpg);
-			image.Mutate(x => x.Resize(Width, Height));
-			image.Save(output.Path, new JpegEncoder {
-				Quality = 80,
-			});
+		[Benchmark(Description = "FreeImage Buffer to Buffer")]
+		public void FreeImageBufferBuffer() {
+			using var ims = new MemoryStream(buffer);
+			using var image = FreeImageBitmap.FromStream(ims);
+			using var resized = new FreeImageBitmap(image, Width, Height);
+			using var oms = new MemoryStream();
+			resized.Save(oms, FREE_IMAGE_FORMAT.FIF_JPEG, FREE_IMAGE_SAVE_FLAGS.JPEG_QUALITYGOOD);
+			oms.ToArray();
 		}
 
-		[Benchmark]
-		public void MagickFromBufferToFile() {
+		[Benchmark(Description = "FreeImage File to File")]
+		public void FreeImageFileFile() {
 			using var output = TestFiles.OutputJpg();
+			using var image = FreeImageBitmap.FromFile(TestFiles.InputJpg);
+			using var resized = new FreeImageBitmap(image, Width, Height);
+			resized.Save(output.Path, FREE_IMAGE_FORMAT.FIF_JPEG, FREE_IMAGE_SAVE_FLAGS.JPEG_QUALITYGOOD);
+		}
+
+		[Benchmark(Description = "ImageMagick Buffer to Buffer")]
+		public void ImageMagickBufferBuffer() {
 			using var image = new MagickImage(buffer) {
 				FilterType = FilterType.Lanczos,
-				Quality = 80
+				Quality = Quality
 			};
 			image.Resize(Width, Height);
-			image.Write(output.Path, MagickFormat.Jpg);
+			using var ms = new MemoryStream();
+			image.Write(ms, MagickFormat.Jpg);
+			ms.ToArray();
 		}
 
-		[Benchmark]
-		public void MagickFromFileToFile() {
+		[Benchmark(Description = "ImageMagick File to File")]
+		public void ImageMagickFromFileToFile() {
 			using var output = TestFiles.OutputJpg();
 			using var image = new MagickImage(TestFiles.InputJpg) {
 				FilterType = FilterType.Lanczos,
-				Quality = 80
+				Quality = Quality
 			};
 			image.Resize(Width, Height);
 			image.Write(output.Path, MagickFormat.Jpg);
 		}
 
-		[Benchmark]
-		public void MagickFromStreamToFile() {
-			using var output = TestFiles.OutputJpg();
-			using var stream = File.OpenRead(TestFiles.InputJpg);
-			using var image = new MagickImage(stream) {
-				FilterType = FilterType.Lanczos,
-				Quality = 80
-			};
-			image.Resize(Width, Height);
-			image.Write(output.Path, MagickFormat.Jpg);
+		[Benchmark(Description = "ImageSharp Buffer to Buffer")]
+		public void ImageSharpBufferBuffer() {
+			using var image = Image.Load(buffer);
+			image.Mutate(x => x.Resize(Width, Height, LanczosResampler.Lanczos3));
+			using var ms = new MemoryStream();
+			image.Save(ms, new JpegEncoder {
+				Quality = Quality
+			});
+			ms.ToArray();
 		}
 
-		[Benchmark(Baseline = true)]
-		public void SharpSharpFromBufferToFile() {
+		[Benchmark(Description = "ImageSharp File to File")]
+		public void ImageSharpFileFile() {
 			using var output = TestFiles.OutputJpg();
+			using var image = Image.Load(TestFiles.InputJpg);
+			image.Mutate(x => x.Resize(Width, Height, LanczosResampler.Lanczos3));
+			image.Save(output.Path, new JpegEncoder {
+				Quality = Quality
+			});
+		}
+
+		[Benchmark(Baseline = true, Description = "SharpSharp Buffer to Buffer")]
+		public void SharpSharpBufferBuffer() {
 			ImagePipeline
 				.FromBuffer(buffer)
 				.Resize(Width, Height)
-				.ToFile(output.Path);
+				.ToBuffer(out var data);
 		}
 
-		[Benchmark]
-		public void SharpSharpFromFileToFile() {
+		[Benchmark(Description = "SharpSharp File to File")]
+		public void SharpSharpFileFile() {
 			using var output = TestFiles.OutputJpg();
 			ImagePipeline
 				.FromFile(TestFiles.InputJpg)
@@ -84,14 +109,26 @@ namespace SharpSharp.Benchmarks {
 				.ToFile(output.Path);
 		}
 
-		[Benchmark]
-		public void SharpSharpFromStreamToFile() {
+		[Benchmark(Description = "SkiaSharp Buffer to Buffer")]
+		public void SkiaSharpBufferBuffer() {
+			using var ims = new MemoryStream(buffer);
+			using var inputStream = new SKManagedStream(ims);
+			using var original = SKBitmap.Decode(inputStream);
+			using var resized = original.Resize(new SKImageInfo(Width, Height), SKBitmapResizeMethod.Lanczos3);
+			using var image = SKImage.FromBitmap(resized);
+			image.Encode(SKEncodedImageFormat.Jpeg, Quality).ToArray();
+		}
+
+		[Benchmark(Description = "SkiaSharp File to File")]
+		public void SkiaSharpFileFile() {
+			using var fs = File.OpenRead(TestFiles.InputJpg);
+			using var inputStream = new SKManagedStream(fs);
+			using var original = SKBitmap.Decode(inputStream);
+			using var resized = original.Resize(new SKImageInfo(Width, Height), SKBitmapResizeMethod.Lanczos3);
 			using var output = TestFiles.OutputJpg();
-			using var stream = File.OpenRead(TestFiles.InputJpg);
-			ImagePipeline
-				.FromStream(stream)
-				.Resize(Width, Height)
-				.ToFile(output.Path);
+			using var oms = File.OpenWrite(output.Path);
+			using var image = SKImage.FromBitmap(resized);
+			image.Encode(SKEncodedImageFormat.Jpeg, Quality).SaveTo(oms);
 		}
 	}
 }
